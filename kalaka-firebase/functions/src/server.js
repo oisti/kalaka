cdef884b71d67e883dfdceb4eb279460a34438bc
Olah Istvan <istvan.olah@magicsolutions.ro>
2019-11-15 23:09:04 +0200

    Announcement post service

---
+++ kalaka-firebase/functions/src/server.js
@@ -0,0 +1,16 @@
+const express       = require("express");
+const bodyParser    = require("body-parser");
+const cors          = require("cors");
+const app           = express();
+
+app.use(cors({origin: true}))
+   .use(bodyParser.json())
+   .use(bodyParser.urlencoded({extended: false}))
+   .use("/announcement", require("./lib/announcement/route"))
+   .get('*', (_,res) => res.status(404)
+                            .json({
+                                success: false, 
+                                data:"Endpoint not found"}));
+
+
+module.exports = app;

274292c790fcb502a5aa3f040cffe363c5d4ae9c
Baliga-Kovács Ágoston <agoston.baliga@magicsolutions.ro>
2019-11-17 11:10:00 +0200

    Header menu

--- kalaka-mobile/src/components/AppHeader.tsx
+++ kalaka-mobile/src/components/AppHeader.tsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { View, StatusBar, StyleSheet, Platform, TouchableOpacity } from "react-native";
-import { Body, Button, Header, Icon, Left, Title, Thumbnail } from "native-base";
+import { View, StatusBar, StyleSheet, Platform, TouchableOpacity, Modal } from "react-native";
+import { Body, Button, Header, Icon, Left, Title, Thumbnail, Drawer, Text } from "native-base";
 import TextInput from "./TextInput";
+import ProfileForm from "./ProfileForm";
 
 interface Props {
   headerText: string;
@@ -11,6 +12,13 @@ interface Props {
 
 
 export default class AppHeader extends Component<Props> {
+  state = {
+    modalVisible: false,
+  };
+
+  setModalVisible = (visible) => {
+    this.setState({modalVisible: visible});
+  }
 
 
   activeHandler = async () => {
@@ -29,6 +37,13 @@ export default class AppHeader extends Component<Props> {
   render() {
     return (
       <View style={styles.androidMargin}>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {}}>
+            <ProfileForm setModalVisible={()=>{this.setModalVisible(false)}}/>
+        </Modal>
         <Header style={styles.headerBackground}>
           <StatusBar barStyle="light-content" />
           <Body style={{ flexDirection: "row" }}>
@@ -42,10 +57,10 @@ export default class AppHeader extends Component<Props> {
               hideLabel
               startAdornment={<Icon active name='search' style={{ color: "lightgray", fontSize: 25, elevation: 5 }} />}
             />
-            <TouchableOpacity>
+            <TouchableOpacity onPress={()=>{this.setModalVisible(true)}}>
               <Thumbnail
                 style={{ width: 40, height: 40, borderRadius: 40 / 2, borderColor: "#F73D10", borderWidth: 2 }}
-                source={{ uri: "https://icon-library.net/images/avatar-icon-png/avatar-icon-png-10.jpg" }} />
+                source={{ uri: "https://scontent-otp1-1.xx.fbcdn.net/v/t31.0-8/920773_461458770604601_404091393_o.jpg?_nc_cat=111&_nc_oc=AQn-IQH8C6G_terg_wc_PFHW1oqxzLUVC-UK0mh9zUT4Snsa_6xgRqRfmo1-DDiZfBo&_nc_ht=scontent-otp1-1.xx&oh=77a0881dc7d1195cca71ce30701efe67&oe=5E57181D" }} />
             </TouchableOpacity>
           </Body>
         </Header>

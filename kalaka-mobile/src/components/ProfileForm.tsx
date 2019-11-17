c7c756322f5814fc3d698dbd43f592bc98ab9395
Baliga-Kovács Ágoston <agoston.baliga@magicsolutions.ro>
2019-11-17 12:01:16 +0200

    profile form

--- kalaka-mobile/src/components/ProfileForm.tsx
+++ kalaka-mobile/src/components/ProfileForm.tsx
@@ -7,36 +7,36 @@ export default class AppHeader extends Component<Props> {
         super(props);
         
         this.state = {
-            availableValue: false,
-            visibleOnMapValue: false,
+            availableValue: true,
+            visibleOnMapValue: true,
         }
     }
   render() {
     return (
       <View style={{height: 350, marginTop: 20}}>
-            <AntDesign name="closecircleo" color="grey" size={15} style={{position: "absolute", right: 10}} onPress={()=>{this.props.setModalVisible()}}/>
+            <AntDesign name="closecircleo" color="grey" size={25} style={{position: "absolute", right: 18}} onPress={()=>{this.props.setModalVisible()}}/>
             <Grid style={{marginTop: 20}}>
                 <Row>
-                    <Col alignItems="center" justifyContent="center" style={{marginTop: 25}}>
+                    <Col alignItems="center" justifyContent="center" style={{marginTop: 45}}>
                         <Row alignItems="center" justifyContent="center">
                             <Thumbnail
                                 style={{ width: 110, height: 110, borderRadius: 110 / 2 }}
                                 source={{ uri: "https://scontent-otp1-1.xx.fbcdn.net/v/t31.0-8/920773_461458770604601_404091393_o.jpg?_nc_cat=111&_nc_oc=AQn-IQH8C6G_terg_wc_PFHW1oqxzLUVC-UK0mh9zUT4Snsa_6xgRqRfmo1-DDiZfBo&_nc_ht=scontent-otp1-1.xx&oh=77a0881dc7d1195cca71ce30701efe67&oe=5E57181D" }} />
                         </Row>
                         <Row alignItems="center" justifyContent="center">
-                            <Text style={{marginBottom: -30}}>
+                            <Text style={{marginBottom: -50}}>
                                 {"Gábor Ervin"}
                             </Text>
                         </Row>
                     </Col>
-                    <Col style={{height: 100, marginTop: 20}} alignItems="center" justifyContent="center">
+                    <Col style={{height: 150, marginTop: 20}} alignItems="center" justifyContent="center">
                         <Row  justifyContent="center">
                             <Col size={3}  justifyContent="center">
                                 <Text style={{ fontSize: 10 }}>
                                     {"Elérhető"}
                                 </Text>
                             </Col>
-                            <Col size={1}  justifyContent="center">
+                            <Col size={1}  justifyContent="center" style={{marginRight: 16}}>
                                 <Switch value={this.state.availableValue} onValueChange={(value)=>{this.setState({availableValue: value})}}/>
                             </Col>
                         </Row>
@@ -46,11 +46,11 @@ export default class AppHeader extends Component<Props> {
                                     {"Térképen láthatóság"}
                                 </Text>
                             </Col>
-                            <Col size={1}  justifyContent="center">
+                            <Col size={1}  justifyContent="center" style={{marginRight: 16}}>
                                 <Switch  value={this.state.visibleOnMapValue} onValueChange={(value)=>{this.setState({visibleOnMapValue: value})}}/>
                             </Col>
                         </Row>
-                        <Row  justifyContent="center">
+                        <Row  justifyContent="center" >
                             <Col  justifyContent="center">
                                 <Text style={{ fontSize: 10 }}>
                                     {"24k elismerés"}
@@ -66,13 +66,13 @@ export default class AppHeader extends Component<Props> {
                         </Row>
                     </Col>
                 </Row>
-                <Row>
-                    <Text style={{ fontSize: 10, marginLeft: 50 }}>
+                <Row style={{marginBottom: -70}}>
+                    <Text style={{ fontSize: 10, marginLeft: 50, marginTop: 20 }}>
                         {"Írj magadról egy rövid bemutatkozót"}
                     </Text>
                 </Row>
                 <Button
-                    style={{width: "80%", marginTop: -150}}
+                    style={{width: "80%", marginTop: -100}}
                         title="Profil szerkesztése"
                         color="rgb(161, 161, 161)"
 
